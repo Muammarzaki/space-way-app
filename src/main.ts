a43d120b9896ea545c590c4de070d46ebce3cc17
muammarzaki <muammarzaki2004@gmail.com>
2025-08-19 21:29:30 +0700

    feat: integrate Prisma with NestJS by adding PrismaService for database management, updating AppModule, installing @prisma/client, updating .gitignore for dist and generated/prisma, configuring Prettier and ESLint, moving app.controller.spec.ts to the test directory, and enhancing main.ts with startup messages and error handling.

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap()
+  .then(() => console.log('Server is running on port http://localhost:3000'))
+  .catch((err) => console.log(err));
